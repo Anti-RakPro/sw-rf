db70b88808d8549bd4252a303ea980ea01ebe296
Anti-RakPro <kvit.d.o@gmail.com>
2024-10-29 18:23:44 +0200

    add: before adding font awersome packagis

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,11 +5,15 @@ import styles from './SideBar.module.css'
 function SideBar() {
 
     const [isToggleSideBar, setToggleSideBar] = useState(false);
+    const [isSideBarModSwitch, setSideBarModSwitch] = useState(true);
 
     function handleToggle() {
         setToggleSideBar(!isToggleSideBar)
     }
-
+    function handleModeSwitch (str){
+        if (str === "All" && !isSideBarModSwitch) {setSideBarModSwitch(!isSideBarModSwitch)}
+        if(str === "Search" && isSideBarModSwitch) {setSideBarModSwitch(!isSideBarModSwitch)}
+    }
 
     // classNames(styles['sideBarBtn'], {`${styles['right']}`: isToggleSideBar})
     return (
@@ -17,6 +21,14 @@ function SideBar() {
             styles['sideBarBlock'],
             isToggleSideBar && styles['sideBar-hidden']
         )}>
+            <h3>Star Wars Characters </h3>
+            <div>
+                <input/>
+                <button>
+                    <FontAwesomeIcon icon="fas fa-search" />
+                </button>
+            </div>
+
             <div className={styles['sideBarBtn']}
                  onClick={handleToggle}>
                 <i className={classNames(
@@ -24,7 +36,29 @@ function SideBar() {
                     isToggleSideBar && styles['right']
                 )}></i>
             </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, voluptate.</p>
+            <div className={classNames(
+               styles['sideBarModSwitch']
+            )}>
+                <button className={classNames(
+                    isSideBarModSwitch && styles['sideBarModSwitchActive']
+                )} onClick={()=>handleModeSwitch('All')}>All</button>
+                <button className={classNames(
+                    !isSideBarModSwitch && styles['sideBarModSwitchActive']
+                )} onClick={()=>handleModeSwitch('Search')}>Search</button>
+            </div>
+            <div className={classNames(
+                styles['sideBarContent'],
+                isToggleSideBar && styles['sideBarContentPosition']
+            )}>
+                <div className={classNames(
+                    !isSideBarModSwitch && styles['sideBarContentPosition']
+                )} > 111 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, voluptate.</div>
+                <div className={classNames(
+                    !isSideBarModSwitch && styles['sideBarContentPosition']
+                )}> 222 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, voluptate.</div>
+
+            </div>
+
         </div>
     )
 
